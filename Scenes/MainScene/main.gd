f548f845e942ea0000bbf957f671f05d4d22a863
Jason Awogu <148569458+AJ2126@users.noreply.github.com>
2025-04-12 11:47:19 +0100

    Updated the player

--- Scenes/MainScene/main.gd
+++ Scenes/MainScene/main.gd
@@ -1,22 +1,24 @@
 extends Node
 
 @onready var main_menu: CanvasLayer = $MainMenu
+@onready var player: CharacterBody2D = $Player
 
 var is_running: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	is_running = player.is_game_running
 
 
 func _on_main_menu_play_pressed() -> void:
 	main_menu.hide()
 	is_running = true
+	player.is_game_running = true
 
 
 func _on_main_menu_exit_pressed() -> void:
